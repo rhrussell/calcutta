99558a2119d590edc7cf5327cb4b36e043ae1513
rhrussell <ryan.russell@aa.com>
2024-07-03 14:57:05 -0500

    Getting this test to pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  if(true){
     expect(true).toBe(true);
-  }
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 });

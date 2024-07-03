18e9c7d3f2cfcb9d4cc4b9d91593c4185945010f
rhrussell <ryan.russell@aa.com>
2024-07-03 14:45:18 -0500

    Getting rid of unnecessary React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { NumberOfPlayersProvider } from "./NumberOfPlayersContext";
 import LeagueForm from "./LeagueForm/LeagueForm";
